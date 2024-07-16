32ef6716e1bfc4301ac211c00dbcdb7f0403ca85
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-07-16 22:31:24 +0300

    Class name starts with UpperCase letters

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""This is the Square Class Module """
+""" Module for Square class"""
 
 
 class Square():
     """ Square class 
-        Arguments:
+        Attributes:
             width: int
             height: int
     """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """ Instantiation method """
+        """ Instantiation of the class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Method that finds perimeter of my square"""
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Prints string representation """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
-    """ Creating square object """
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
